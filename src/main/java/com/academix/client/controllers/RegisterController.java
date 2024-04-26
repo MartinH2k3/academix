2376f732a980c976ba874cacc9a0a96184cdfc31
majo955 <mario.vazny@gmail.com>
2024-04-26 14:49:06 +0200

    odstranenie bubbles znova

--- src/main/java/com/academix/client/controllers/RegisterController.java
+++ src/main/java/com/academix/client/controllers/RegisterController.java
@@ -17,18 +17,6 @@ import server.logging.Logging;
 
 public class RegisterController {
     private MainApplication mainApplication;
-    @FXML
-    private Polygon invalidPasswordBubble;
-    @FXML
-    private Polygon invalidConfirmPasswordBubble;
-    @FXML
-    private Polygon invalidUsernameBubble;
-    @FXML
-    private Text invalidPasswordText;
-    @FXML
-    private Text invalidConfirmPasswordText;
-    @FXML
-    private Text invalidUsernameText;
 
     @FXML
     private TextField usernameTextfield;
@@ -66,27 +54,20 @@ public class RegisterController {
         this.mainApplication = mainApplication;
     }
 
+    @FXML
     public void register(ActionEvent actionEvent) {
-        hidePasswordBubble(null);
-        hideUsernameBubble(null);
-        hideConfirmPasswordBubble(null);
         String username = usernameTextfield.getText();
         String password = passwordPasswordField.getText();
 
+
         if (!FormatCheck.isValidUsername(username)){
-            invalidUsernameBubble.setVisible(true);
-            invalidUsernameText.setVisible(true);
             return;
         }
         if(passwordPasswordField.getCharacters().isEmpty()){
-            invalidPasswordBubble.setVisible(true);
-            invalidPasswordText.setVisible(true);
             return;
         }
 
         if(!confirmPasswordField.getText().equals(passwordPasswordField.getText())){
-            invalidConfirmPasswordBubble.setVisible(true);
-            invalidConfirmPasswordText.setVisible(true);
             return;
         }
         String response;
@@ -109,22 +90,6 @@ public class RegisterController {
                 Logging.getInstance().logException(e, "Pri registrácii nastala chyba.");
             }
         }
-    }
-
-    public void hideUsernameBubble(MouseEvent mouseEvent) {
-        invalidUsernameBubble.setVisible(false);
-        invalidUsernameText.setVisible(false);
-
-    }
-
-    public void hidePasswordBubble(MouseEvent mouseEvent) {
-        invalidPasswordBubble.setVisible(false);
-        invalidPasswordText.setVisible(false);
-    }
-
-    public void hideConfirmPasswordBubble(MouseEvent mouseEvent) {
-        invalidConfirmPasswordBubble.setVisible(false);
-        invalidConfirmPasswordText.setVisible(false);
     }
 
     // You can add more methods and fields as needed
